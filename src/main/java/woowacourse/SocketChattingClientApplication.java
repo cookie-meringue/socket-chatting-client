5b0a62a220f276d707ffabb0fee644e1680c910b
KimDaehyeon <daehyeon3351@gmail.com>
2025-05-29 11:07:41 +0900

    settings: init java application
    - Java version 17.

---
+++ src/main/java/woowacourse/SocketChattingClientApplication.java
@@ -0,0 +1,13 @@
+package woowacourse;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import woowacourse.udp.echo.UdpEchoClient;
+import woowacourse.udp.file.UdpFileClient;
+
+public class SocketChattingClientApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
